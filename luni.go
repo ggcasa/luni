340f2857749afe201d703d7812c88eef98537059
Gigi <111974834+GGCasa@users.noreply.github.com>
2023-11-24 12:57:45 +0000

    Update luni.go

--- luni.go
+++ luni.go
@@ -1,21 +1,10 @@
 // Package luni key value store
 package luni
 
-import (
-	"net"
-	"time"
+type Ddsp struct {
+	Host string
+}
+
+var (
+	dsp = make(map[string]Ddsp)
 )
-
-// Conn
-type Conn interface {
-	DialTcp(ip net.IP, p string) (b bool, err error)
-	DialUdp(ip net.IP, p string) (b bool, err error)
-}
-
-// Zi
-type Zi interface {
-	Add(k []byte, t time.Time) (b bool, err error)
-	Get(k []byte) (n int, err error)
-	Find(k []byte, s string)
-	Delete(k []byte) (s string, err error)
-}
